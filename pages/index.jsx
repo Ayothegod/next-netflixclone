4387e3c90076ca852aa85b5de828d182775829a8
gboye333 <gboye333@gmail.com>
2022-10-26 21:00:40 +0100

    final commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 //: NextPage
 import { useSession, signIn, signOut } from "next-auth/react"
+import image from '../public/netflix-3.svg'
 
 const Home = () => {
   const { data:session} = useSession()
@@ -17,21 +18,29 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=" text-white flex w-full flex-1 flex-col items-center justify-center px-20 text-center ">
-      {session && <> Signed in as {session.user.email} <br />
-        <button className='bg-red-500 px-8  ' onClick={() => signOut()}>Sign out</button></>
-        
-        }
+      <header className='sm:flex justify-between items-center my-2 px-4  w-full'>
+        <div className='text-white font-bold text-2xl'>
+          <Image
+      src={image}
+      alt="Picture of the author"
+      width={100}
+      height={90}
+    />
+        </div>
+        <div className='flex text-center'>
+          {!session ? <>Not signed in <br />
+      <button className='bg-red-500 px-6 py-2 rounded'  onClick={() => signIn()}>Sign in</button></> : <div className='text-white'> Signed in as {session.user.email} <br />
+        <button className='bg-red-500 px-6 py-2 rounded ' onClick={() => signOut()}>Sign out</button></div>}
+        </div>
+      </header>
 
-      {!session && <>Not signed in <br />
-      <button className='bg-red-500 px-4'  onClick={() => signIn()}>Sign in</button></>}
-
-        <h1 className='text-5xl font-medium '>Unlimited movies, TV shows, and more.</h1>
-        <p className='text-3xl my-4'>Watch anywhere. Cancel anytime.</p>
-        <p className='my-2'>Ready to watch? Enter your email to create or restart your membership.</p>
-        <div className=' flex bg-red-400'>
-        <input type="text" placeholder='Enter Address' className='border-2 py-3 px-2 w-96 '/>
-        <button className='bg-red-500 px-8 '>Get Started</button>
+      <main className=" text-white flex w-full flex-1 flex-col items-center justify-center  text-center ">
+        <h1 className='text-2xl sm:text-5xl font-medium '>Unlimited movies, TV shows, and more.</h1>
+        <p className=' sm:text-3xl sm:my-4'>Watch anywhere. Cancel anytime.</p>
+        <p className='leading-none my-2'>Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className=' sm:flex '>
+        <input type="text" placeholder='Enter Address' className='py-1 px-4   sm:px-2 sm:w-96 '/>
+        <button className='p-1 bg-red-500 rounded mt-2 sm:mt-0 sm:px-2 sm:py-3 '>Get  Started</button>
         </div>
       </main>
     </div>
